36b2e7038e9e7a56f3d2c9afe9e44ccf47686ecf
Andrew Kett <ajkett@gmail.com>
2013-09-10 22:24:21 +1000

    more configuration options in admin and more description. Only use customer address to search if enabled in admin

--- app/code/local/MageBrews/Locator/Model/Search.php
+++ app/code/local/MageBrews/Locator/Model/Search.php
@@ -22,6 +22,7 @@ class MageBrews_Locator_Model_Search extends MageBrews_Locator_Model_Search_Abst
 {
     const XML_SEARCH_OVERRIDES_PATH = "locator_settings/search/overrides_enabled";
     const XML_SEARCH_USEDEFAULT_PATH = "locator_settings/search/use_default_search";
+    const XML_SEARCH_USE_CUSTOMER_ADDRESS = "locator_settings/search/use_customer_address";
 
     var $params = array();
 
@@ -79,23 +80,25 @@ class MageBrews_Locator_Model_Search extends MageBrews_Locator_Model_Search_Abst
         }
         else{
 
-            //if customer is logged in and they have an address use that
-            $session = Mage::getSingleton('customer/session');
-            if($session->isLoggedIn() && $session->getCustomer()->getDefaultBilling()){
-                $addressId = $session->getCustomer()->getDefaultBilling();
+            if(Mage::getStoreConfig(self::XML_SEARCH_USE_CUSTOMER_ADDRESS)){
+                //if customer is logged in and they have an address use that
+                $session = Mage::getSingleton('customer/session');
+                if($session->isLoggedIn() && $session->getCustomer()->getDefaultBilling()){
+                    $addressId = $session->getCustomer()->getDefaultBilling();
 
-                $address = Mage::getModel('customer/address')->load($addressId);
-                $street = $address->getStreet();
+                    $address = Mage::getModel('customer/address')->load($addressId);
+                    $street = $address->getStreet();
 
-                $search = @$street[0].' '.@$street[1].', '.$address->getCity().', '.$address->getRegion().', '.$address->getPostcode().', '.$address->getCountry();
-                $searchModel = Mage::getModel('magebrews_locator/search_point_string');
-                $newParams = array('s'=>$search, 'distance'=>500);
+                    $search = @$street[0].' '.@$street[1].', '.$address->getCity().', '.$address->getRegion().', '.$address->getPostcode().', '.$address->getCountry();
+                    $searchModel = Mage::getModel('magebrews_locator/search_point_string');
+                    $newParams = array('s'=>$search, 'distance'=>500);
 
-                //if there are results close to the customer use that
-                //otherwise just fallback to default search
-                if($searchModel->search($newParams)->getItems()){
-                    $this->params = array_merge($newParams, $this->params);
-                    return $searchModel;
+                    //if there are results close to the customer use that
+                    //otherwise just fallback to default search
+                    if($searchModel->search($newParams)->getItems()){
+                        $this->params = array_merge($newParams, $this->params);
+                        return $searchModel;
+                    }
                 }
             }
 
