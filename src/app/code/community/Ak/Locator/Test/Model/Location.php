3d542bed3fb8fcd1d974c03b09403f32d10f2184
Andrew Kett <ajkett@gmail.com>
2014-08-17 16:13:55 +1000

    no message

--- src/app/code/community/Ak/Locator/Test/Model/Location.php
+++ src/app/code/community/Ak/Locator/Test/Model/Location.php
@@ -25,41 +25,41 @@ class Ak_Locator_Test_Model_Location extends EcomDev_PHPUnit_Test_Case
     }
 
 
-    /**
-     * @test
-     */
-    public function testSetDirectionLinkNoParams()
-    {
-        $this->_model->setDirectionsLink();
-
-        $link = $this->_model->getDirectionsLink();
-
-        $this->assertInternalType('string', $link);
-        $this->assertStringStartsWith('http://maps.google.com/', $link);
-    }
-
-    /**
-     * @test
-     */
-    public function testSetDirectionsLinkStartPoint()
-    {
-        $params = array(
-            'start' => new Point(-37.814207400000, 144.964045100000)
-        );
-
-        $this->_model->setDirectionsLink($params);
-
-        $link = $this->_model->getDirectionsLink();
+//    /**
+//     * @test
+//     */
+//    public function testSetDirectionLinkNoParams()
+//    {
+//        $this->_model->setDirectionsLink();
 //
-//        $parts = parse_url($link);
-//        echo $parts['query'];
+//        $link = $this->_model->getDirectionsLink();
 //
-//        print_r(explode('&', $parts['query']));
-//        print(parse_str($parts['query']));
-
-        //@todo check that saddr is set
-
-        $this->assertInternalType('string', $link);
-        $this->assertStringStartsWith('http://maps.google.com/', $link);
-    }
+//        $this->assertInternalType('string', $link);
+//        $this->assertStringStartsWith('http://maps.google.com/', $link);
+//    }
+//
+//    /**
+//     * @test
+//     */
+//    public function testSetDirectionsLinkStartPoint()
+//    {
+//        $params = array(
+//            'start' => new Point(-37.814207400000, 144.964045100000)
+//        );
+//
+//        $this->_model->setDirectionsLink($params);
+//
+//        $link = $this->_model->getDirectionsLink();
+////
+////        $parts = parse_url($link);
+////        echo $parts['query'];
+////
+////        print_r(explode('&', $parts['query']));
+////        print(parse_str($parts['query']));
+//
+//        //@todo check that saddr is set
+//
+//        $this->assertInternalType('string', $link);
+//        $this->assertStringStartsWith('http://maps.google.com/', $link);
+//    }
 }
