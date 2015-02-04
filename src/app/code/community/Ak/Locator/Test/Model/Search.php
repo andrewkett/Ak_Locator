7ca8c0d82dabdaf85b0f6bb8fc5f73f5ea803fdf
Andrew Kett <ajkett@gmail.com>
2015-02-04 21:44:05 +1100

    more tests

--- src/app/code/community/Ak/Locator/Test/Model/Search.php
+++ src/app/code/community/Ak/Locator/Test/Model/Search.php
@@ -39,7 +39,7 @@ class Ak_Locator_Test_Model_Search extends EcomDev_PHPUnit_Test_Case
       */
     public function testLatLongSearchResultInstance()
     {
-        $params = array('lat'=>'-37.81420740.0000', 'long'=>'144.964045100000');
+        $params = array('lat'=>-37.814207400000, 'long'=>144.964045100000);
         $this->assertInstanceOf('Ak_Locator_Model_Resource_Location_Collection', $this->_model->search($params));
 
     }
@@ -51,5 +51,35 @@ class Ak_Locator_Test_Model_Search extends EcomDev_PHPUnit_Test_Case
     {
         $params = array('point'=> new Point(-37.814207400000, 144.964045100000));
         $this->assertInstanceOf('Ak_Locator_Model_Resource_Location_Collection', $this->_model->search($params));
+    }
+
+    /**
+     * @test
+     * @loadFixture
+     */
+    public function testLatLongSearchDistance()
+    {
+        $params = array(
+            'lat'=>-37.814207400000,
+            'long'=>144.964045100000,
+            'distance' => 5
+        );
+
+        $this->assertEquals(2, $this->_model->search($params)->count());
+    }
+
+    /**
+     * @test
+     * @loadFixture
+     */
+    public function testLatLongSearchEnabled()
+    {
+        $params = array(
+            'lat'=>-37.814207400000,
+            'long'=>144.964045100000,
+            'distance' => 20
+        );
+
+        $this->assertEquals(2, $this->_model->search($params)->count());
     }
 }
